ed03679b306af251b2a887b8c9838b26e4c24c84
IvanOrsh <ksironov@mail.ru>
2023-07-07 02:45:39 +0300

    test: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,15 @@
 import * as jwtPromisified from './jwt-promisified'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('./jwt-promisified', () => ({
+  __esModule: true,
+  async jwtSignPromisified (): Promise<string> {
+    return new Promise(resolve => {
+      resolve('accessToken')
+    })
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct', async () => {
     const sut = new JwtAdapter('secret')
@@ -10,5 +19,11 @@ describe('Jwt Adapter', () => {
       { id: 'any_id' },
       'secret'
     )
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('accessToken')
   })
 })
