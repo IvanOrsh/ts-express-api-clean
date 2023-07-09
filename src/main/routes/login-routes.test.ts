89e7af99783bee28c4a87bfd18e4332c74056599
IvanOrsh <ksironov@mail.ru>
2023-07-09 11:29:52 +0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,14 +1,18 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 describe('Login Routes', () => {
+  let accountCollection: Collection
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGODB_URI as string)
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -32,6 +36,24 @@ describe('Login Routes', () => {
       expect(name).toBe('Abdullah')
       expect(email).toBe('abdullah@email.com')
       expect(password).toBeTruthy()
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return an 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Abdullah',
+        email: 'abdullah@email.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'abdullah@email.com',
+          password: '123'
+        })
+        .expect(200)
     })
   })
 })
