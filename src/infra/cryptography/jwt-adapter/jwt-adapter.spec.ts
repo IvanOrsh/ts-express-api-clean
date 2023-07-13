ebdb5956a9a3626267655657f599e5cc9efa187a
IvanOrsh <ksironov@mail.ru>
2023-07-13 13:00:18 +0300

    refactor: separate tests in describe blocks

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -15,28 +15,30 @@ const makeSut = (): JwtAdapter => {
 }
 
 describe('Jwt Adapter', () => {
-  test('Should call sign with correct', async () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwtPromisified, 'jwtSignPromisified')
-    await sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith(
-      { id: 'any_id' },
-      'secret'
-    )
-  })
+  describe('sign()', () => {
+    test('Should call sign with correct', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwtPromisified, 'jwtSignPromisified')
+      await sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith(
+        { id: 'any_id' },
+        'secret'
+      )
+    })
 
-  test('Should return a token on sign success', async () => {
-    const sut = makeSut()
-    const accessToken = await sut.encrypt('any_id')
-    expect(accessToken).toBe('accessToken')
-  })
+    test('Should return a token on sign success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_id')
+      expect(accessToken).toBe('accessToken')
+    })
 
-  test('Should throw if sign throw', async () => {
-    const sut = makeSut()
-    jest.spyOn(jwtPromisified, 'jwtSignPromisified').mockReturnValueOnce(new Promise((resolve, reject) => {
-      reject(new Error())
-    }))
-    const promise = sut.encrypt('any_id')
-    await expect(promise).rejects.toThrow()
+    test('Should throw if sign throw', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwtPromisified, 'jwtSignPromisified').mockReturnValueOnce(new Promise((resolve, reject) => {
+        reject(new Error())
+      }))
+      const promise = sut.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
